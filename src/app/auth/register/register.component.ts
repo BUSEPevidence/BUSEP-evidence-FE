a32db5860384622ccbeb8d271388fad3d3e0dcc4
mihajlo182savic <savic.ra182.2019@uns.ac.rs>
2023-05-24 22:14:24 +0200

    Validation for login and register

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -9,7 +9,15 @@ import { RegisterUser } from '../login/model/RegisterUser';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-
+  firstname: string = ''
+  username: string = ''
+  password: string = ''
+  repeater: string = ''
+  lastname: string = ''
+  address: string = ''
+  city: string = ''
+  state: string = ''
+  number: string = ''
   constructor(private router: Router,private authService : AuthService) { }
   public user: RegisterUser = {
     username: '',
@@ -24,7 +32,151 @@ export class RegisterComponent {
     adminApprove: false
   };
   public canReg : boolean = true;
-
+  onFirstnameChange() {
+    // Perform action on text change
+    //console.log('Firstname changed:', this.firstname);
+  }
+  onLastnameChange() {
+    // Perform action on text change
+    //console.log('Lastname changed:', this.lastname);
+  }
+  onAddressChange() {
+    // Perform action on text change
+    //console.log('Address changed:', this.address);
+  }
+  onUsernameChange() {
+    // Perform action on text change
+    //console.log('Username changed:', this.username);
+  }
+  onPasswordChange() {
+    // Perform action on text change
+    //console.log('Password changed:', this.password);
+  }
+  onCityChange() {
+    // Perform action on text change
+    //console.log('City changed:', this.city);
+  }
+  onStateChange() {
+    // Perform action on text change
+    //console.log('State changed:', this.state);
+  }
+  onNumberChange() {
+    // Perform action on text change
+    //console.log('Validate changed:', this.validate());
+  }
+  validateUsername() : boolean{
+    const usernamePattern = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
+    if(!this.username.match(usernamePattern)){
+      return false
+    } 
+    return true
+  }
+  validatePassword() : boolean{
+    const passwordPattern = /^(?=.*\d).+$/;
+    if(!this.password.match(passwordPattern)){
+      return false
+    } 
+    return true
+  }
+  validateFirstname() : boolean{
+    const bigFirst = /^[A-Z][a-z]*$/;
+    if(!this.firstname.match(bigFirst)){
+      return false
+    } 
+    return true
+  }
+  validateLastname() : boolean{
+    const bigFirst = /^[A-Z][a-z]*$/;
+    if(!this.lastname.match(bigFirst)){
+      return false
+    } 
+    return true
+  }
+  validateNumber() : boolean{
+    const numbers = /^\d+$/
+    if(!this.number.match(numbers)){
+      return false
+    } 
+    return true
+  }
+  validateAddress() : boolean{
+    const addressPattern = /^[A-Z][A-Za-z]+(?:\s[A-Z][A-Za-z]+)*$/
+    if(!this.address.match(addressPattern)){
+      return false
+    } 
+    return true
+  }
+  validateCity() : boolean{
+    const addressPattern = /^[A-Z][A-Za-z]+(?:\s[A-Z][A-Za-z]+)*$/
+    if(!this.city.match(addressPattern)){
+      return false
+    } 
+    return true
+  }
+  validateState() : boolean{
+    const addressPattern = /^[A-Z][A-Za-z]+(?:\s[A-Z][A-Za-z]+)*$/
+    if(!this.state.match(addressPattern)){
+      return false
+    } 
+    return true
+  }
+  validateRepeat() : boolean{
+    if(this.password != this.repeater)
+    {
+      console.log("razliciti passwordi")
+      return false;
+    }
+    return true
+  }
+  validate() : boolean{
+    if(!(this.firstname != '' && this.lastname != '' && this.address != '' && this.username != '' && this.password != '' && this.city != '' && this.state != '' && this.number != ''))
+    {
+      return false
+    }
+    const usernamePattern = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
+    const bigFirst = /^[A-Z][a-z]*$/;
+    const passwordPattern = /^(?=.*\d).+$/;
+    const addressPattern = /^[A-Z][A-Za-z]+(?:\s[A-Z][A-Za-z]+)*$/
+    const numbers = /^\d+$/
+    if(!this.username.match(usernamePattern)){
+      console.log("puko username")
+      return false
+    } 
+    if(!this.firstname.match(bigFirst)) {
+      console.log("puko firstname")
+      return false;
+    }
+    if(!this.lastname.match(bigFirst)){
+      console.log("puko lastname")
+      return false;
+    } 
+    if(!this.password.match(passwordPattern)) {
+      console.log("puko password")
+      return false;
+    } 
+    if(!this.state.match(addressPattern)) {
+      console.log("puko state")
+      return false;
+    } 
+    if(!this.city.match(addressPattern)) {
+      console.log("puko city")
+      return false;
+    } 
+    if(!this.address.match(addressPattern)) {
+      console.log("puko address")
+      return false;
+    } 
+    if(!this.number.match(numbers)) {
+      console.log("puko number")
+      return false;
+    } 
+    if(this.password != this.repeater)
+    {
+      console.log("razliciti passwordi")
+      return false;
+    }
+    return true
+  }
   public roleList?: string[];
   public findChecked(){
     const firstNameInput = document.querySelector<HTMLInputElement>('#firstname');
